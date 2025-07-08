29f2e36a944390f7fe0fafaaa52977e6fc4bff72
ZyrusAlvez <zyrusalvez13@gmail.com>
2025-07-08 19:52:35 +0800

    refactor sleep

--- main.py
+++ main.py
@@ -11,22 +11,19 @@ def get_local_time():
 
 # Main loop function
 def run_scraper(url_list):
-    for _ in range(24):
-        for url in url_list:
-            try:
-                status, name = tvgarden_scraper(url)
-            except Exception:
-                status = "DOWN"
-                name = "Unknown"
+    for url in url_list:
+        try:
+            status, name = tvgarden_scraper(url)
+        except Exception:
+            status = "DOWN"
+            name = "Unknown"
 
-            supabase.table("tv.garden").insert({
-                "status": status,
-                "name": name,
-                "timestamp": get_local_time(),
-                "url": url
-            }).execute()
-
-        time.sleep(3600)  # Sleep for 1 hour
+        supabase.table("tv.garden").insert({
+            "status": status,
+            "name": name,
+            "timestamp": get_local_time(),
+            "url": url
+        }).execute()
 
 if __name__ == "__main__":
     urls = [
